7d264c468f2c8e20876f6e3a229d5ebffc2c9224
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-24 13:56:02 +0100

    add email sign-up and include on sign-in and sign-up page

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
               ...snapShot.data(),
             },
           });
+          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });
