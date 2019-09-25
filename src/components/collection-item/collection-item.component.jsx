738341a4d56bf4d6d2d8c318abbf760a5a219ba2
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-25 11:43:34 +0100

    include styling and extend collection item component with add to cart custom button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss'
 
 
-const CollectionItem = ({id, name, price, imageUrl }) =>  (
+const CollectionItem = ({id, name, price, imageUrl, }) =>  (
         <div className='collection-item'>
             <div className='image'
             style={{
@@ -15,6 +17,7 @@ const CollectionItem = ({id, name, price, imageUrl }) =>  (
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
+            <CustomButton inverted>Add to cart</CustomButton>
         </div>
     )
 
