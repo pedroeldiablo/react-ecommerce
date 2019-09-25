6467ce280ca6fb8187521e908752087436d421b9
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-25 14:58:26 +0100

    updating application with reselect, replaced cart selectors with memoized selectors to prevent rerendering when state not used in cart updates

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
- import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -13,10 +14,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
     </div>
 )
 
-const mapStateToProps = ({ cart: {cartItems}}) => ({
-    itemCount: cartItems.reduce(
-        (accumulatedQuantity, cartItem) => 
-        accumulatedQuantity + cartItem.quantity, 0) 
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({
