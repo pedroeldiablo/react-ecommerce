923a9b0cf327a47fe3d6bbf5d394db68bb3267e6
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-25 01:10:44 +0100

    Updating app to redirect from signup-in to home if user is signed in and splitting const types out for consistancy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 
@@ -51,15 +51,27 @@ class App extends Component {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
           {/* <Route exact path="/shop/hats" component={HatsPage} /> */}
-          <Route exact path='/signin' component={SignInAndSignUpPage} />
+          <Route 
+          exact 
+          path='/signin' 
+          render={() => 
+            this.props.currentUser ? 
+            ( <Redirect to='/' /> ) :
+            ( <SignInAndSignUpPage />)
+          }
+          />
         </Switch>
       </div>
     );
   } 
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
