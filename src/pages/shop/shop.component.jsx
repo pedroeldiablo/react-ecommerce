f9b9c6d75851a24e2418dc086fdc1652e9a229d7
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-26 15:56:46 +0100

    moving shop data and structuring with redux in reducer and selector

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-import SHOP_DATA from './shop.data';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopCollection } from '../../redux/shop/shop.selector';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            collections: SHOP_DATA,
-        }
-    }
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                <h1>Collection</h1>
-                { collections.map(( { id, ...otherCollectionProps } ) => (
-                    
-                    <CollectionPreview key={id} { ...otherCollectionProps }/>
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        <h1>Collection</h1>
+        { collections.map(( { id, ...otherCollectionProps } ) => (
+            <CollectionPreview key={id} { ...otherCollectionProps }/>
+        ))}
+    </div>
+)
 
-                ))}
-        </div>
-        )
-    }
-}
-
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollection,
+  
+  });
+  
+  
+  export default connect(mapStateToProps)(ShopPage);
