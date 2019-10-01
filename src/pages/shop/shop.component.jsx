5f48ca9d9c7fa60a73fa0463574a4a715bebf5c1
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-01 19:52:33 +0100

    convert collections snapshot to map

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => { 
-    return (
-    <div className="shop-page">
-        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    </div>
-)
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+class ShopPage extends Component {
+    unSubscribeFromSnapShot = null;
+
+    componentDidMount() {
+        const collectionRef = firestore.collection('collections');
+        collectionRef.onSnapshot(async snapshot => {
+            console.log(snapshot);
+            convertCollectionsSnapshotToMap(snapshot);
+        })
+
+    }
+
+    render() {
+        const { match } = this.props;
+        return (
+            <div className="shop-page">
+                <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            </div>
+        )
+
+    }   
 }
 
   
