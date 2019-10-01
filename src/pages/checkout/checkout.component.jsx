b28fb6c31f0b4864723afb3e98d343319e5d1135
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-01 11:29:28 +0100

    adding stripe payment button and linking to Stripe on the frontend

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss';
 
@@ -33,6 +34,14 @@ const CheckoutPage = ({cartItems, cartTotal }) => (
         <div className='total'>
             Total: ${cartTotal}
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CSV: 123
+            from https://stripe.com/docs/testing#cards
+
+        </div>
+        <StripeCheckoutButton price={cartTotal} />
    </div>
 
 );
