9a61e0c8114aed7460abe643343ddac6e429141f
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-07 17:26:45 +0100

    update shop component to be functional with useEffect hook

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -8,15 +8,11 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/colllection.container';
 
-class ShopPage extends Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  
-  }
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
     return (
       <div className='shop-page'>
         <Route
@@ -30,7 +26,6 @@ class ShopPage extends Component {
         />
       </div>
     );
-  }
 }
 
 const mapDispatchToProps = dispatch => ({
