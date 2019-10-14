438a4c6fb6e2a2ca9ad80dcb142f92f209c55db4
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-14 12:02:09 +0100

    splitting bundle using lazy and suspense to reduce unneccessary downloads

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import './menu-item.styles.scss';
+import { MenuItemContainer } from './menu-item.styles';
+
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <MenuItemContainer onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div 
             className="background-image" style={{ 
             backgroundImage: `url(${imageUrl})` }}
@@ -13,7 +14,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
             <h1 className="title">{ title.toUpperCase() }</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
-    </div>
+    </MenuItemContainer>
 );
 
 export default withRouter(MenuItem);
