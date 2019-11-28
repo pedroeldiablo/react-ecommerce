cc3026fa9b086bf517ca93dd12fc3865050fd573
Peter Williams <pedroeldiablo@yahoo.com>
2019-11-28 14:42:39 +0000

    exploring adding a carousel

--- client/src/pages/contact/contact.component.jsx
+++ client/src/pages/contact/contact.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import SimpleSlider from '../../components/simple-slider/simple-slider.component';
+
+
 import './contact.styles.scss';
 
 const ContactPage = () => {
@@ -9,7 +12,8 @@ const ContactPage = () => {
     return (
 
     <div className="contact-page">
-        <h2 className='title'>Contact</h2>   
+        <h2 className='title'>Contact</h2> 
+        <SimpleSlider />  
         <p>Taking the ecommerce project from course <a href="https://www.udemy.com/course/complete-react-developer-zero-to-mastery/">Complete React Developer Zero to Mastery/</a> as a starting point this project explores some of the structured approaches to buildiing a complex app with react.</p>
         <p>For more projects to get in touch visit <a href="http://www.peternicholaswilliams.com/">www.peternicholaswilliams.com/</a></p>
         <p>Or see <a href="https://github.com/pedroeldiablo/react-ecommerce">Github</a> to see how it's coded.</p>
